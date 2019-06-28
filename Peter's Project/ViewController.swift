fe6bae57a86f2741b4831b2dbf0b7de67a7316cb
Peter Sirany <peterCFII@hotmail.com>
2019-06-28 13:26:00 +0200

    Initial Commit

---
+++ Peter's Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Peter's Project
+//
+//  Created by Peter Sirany on 6/28/19.
+//  Copyright © 2019 Peter Sirany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Peter's ProjectTests/Peter_s_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Peter_s_ProjectTests.swift
+//  Peter's ProjectTests
+//
+//  Created by Peter Sirany on 6/28/19.
+//  Copyright © 2019 Peter Sirany. All rights reserved.
+//
+
+import XCTest
+@testable import Peter_s_Project
+
+class Peter_s_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
